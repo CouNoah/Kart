41c46e35910f0358b564f51ebb3c633acd272ff4
a-guy772 <jojo772jojo772jojo@gmail.com>
2023-09-07 16:22:47 +0200

    some changes

--- app/src/main/java/ch/hevs/ss1/MyKartRemote.java
+++ app/src/main/java/ch/hevs/ss1/MyKartRemote.java
@@ -111,7 +111,7 @@ public class MyKartRemote extends AbstractKartControlActivity implements KartLis
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 Log.d("SEEKBAR", "move");
-                sw.setRotation(kart.getSteeringPosition());
+                sw.setRotation(kart.getSteeringPosition()-35);
                 //angleText.setText(" Angle: " + i + String.valueOf(kart.getSteeringPosition()) + String.valueOf(kart.getSteeringPositionNormalized()));
                 kart.setSteeringTargetPosition(i);
 
